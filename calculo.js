7080b965601deec2d5f6845fd63cf9140c276940
Bruno Silva <brunoeduardo374@gmail.com>
2023-05-16 22:18:57 -0300

    Primeiro

---
+++ calculo.js
@@ -0,0 +1,20 @@
+
+var multa = 4;
+var limite = 50;
+var e
+var resultado 
+
+function Calcular(){
+    var p = Number(document.getElementById('peso').value);
+
+    if(p > limite){
+        e = p - limite;
+        valorMulta = e * multa;
+        resultado = document.getElementById('multa').innerHTML = `O valor da multa é de ${valorMulta}R$ e o peso foi de ${e} Kg`;
+         
+    }else{
+        resultado = document.getElementById('sem-multa').innerHTML = `Não houve multa, o peso do peixe não atingiu o excesso`;
+        
+    }
+}
+
